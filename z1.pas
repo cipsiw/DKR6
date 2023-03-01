82d2d968e5923f082ebc19ec1c061b48bea764b1
cipsiw <118716892+cipsiw@users.noreply.github.com>
2023-03-01 16:18:22 +0300

    Add files via upload

--- z1.pas
+++ z1.pas
@@ -1,112 +1,116 @@
 ﻿uses Crt;
+
 const
-MAX = 100;
+  MAX = 100;
+
 type
-tInf = Integer;
-tStack = array [1..MAX] of tInf;
+  tInf = Integer;
+  tStack = array [1..MAX] of tInf;
 
 var
-top: Integer;
-Stack: tStack;
+  top: Integer;
+  Stack: tStack;
 
-procedure Push(var Stack : tStack; data : tInf);
+procedure Push(var Stack: tStack; data: tInf);
 begin
-if top = MAX then
-Writeln('Ошибка')
-else begin
-inc(top);
-Stack[top] := data;
-end;
+  if top = MAX then
+    Writeln('Ошибка')
+  else begin
+    inc(top);
+    Stack[top] := data;
+  end;
 end;
 
-function Pop(var Stack : tStack) : tInf;
+function Pop(var Stack: tStack): tInf;
 var
-Res: tInf;
+  Res: tInf;
 begin
-if top = 0 then
-Writeln('Ошибка')
-else begin
-Res := Stack[top];
-dec(top);
-Clrscr;
-end;
-Pop := Res;
-Writeln('Элемент: ', Res);
+  if top = 0 then
+    Writeln('Ошибка')
+  else begin
+    Res := Stack[top];
+    dec(top);
+    Clrscr;
+  end;
+  Pop := Res;
+  Writeln('Элемент: ', Res);
 end;
 
-procedure Print(Stack : tStack);
+procedure Print(Stack: tStack);
 var
-i: Integer;
+  i: Integer;
 begin
-if top = 0 then
-WriteLn('Стек пуст.')
-else begin
-for i := 1 to top do
-Write(Stack[i], ' ');
-WriteLn;
-end;
-WriteLn;
+  if top = 0 then
+    WriteLn('Стек пуст.')
+  else begin
+    for i := 1 to top do
+      Write(Stack[i], ' ');
+    WriteLn;
+  end;
+  WriteLn;
 end;
 
-procedure addToStack(var Stack : tStack);
+procedure addToStack(var Stack: tStack);
 var
-data : tInf;
+  data: tInf;
 begin
-Clrscr;
-textcolor(green);
-Write('Введите значение добавляемого элемента: '); ReadLn(data);
-Push(Stack, data);
-WriteLn;
+  Clrscr;
+  textcolor(green);
+  Write('Введите значение добавляемого элемента: '); ReadLn(data);
+  Push(Stack, data);
+  WriteLn;
 end;
 
-procedure clearStack(var Stack : tStack);
+procedure clearStack(var Stack: tStack);
 begin
-top := 0;
-Clrscr;
-textcolor(red);
-WriteLn('Стек очищен');
+  top := 0;
+  Clrscr;
+  textcolor(red);
+  WriteLn('Стек очищен');
 end;
 
 var
-ch : char;
+  ch: char;
 
 begin
-top := 0;
-repeat
-textcolor(white);
-Writeln('Программа для работы со стеком ');
-Writeln('Выберите желаемое действие:');
-textcolor(white);
-Writeln('1) Добавить элемент');
-textcolor(cyan);
-Writeln('2) Вывод стека');
-textcolor(cyan);
-Writeln('3) Проверка стека на пустоту');
-textcolor(cyan);
-Writeln('4) Очистить стек');
-textcolor(red);
-Writeln('5) Взять элемент из Стека');
-textcolor(red);
-Writeln('6) Выход');
-writeln;
-readln(ch);
-case ch of 
-  '1' : addToStack(Stack);
-  '2' : begin
+  top := 0;
+  repeat
+    textcolor(white);
+    Writeln('Программа для работы со стеком ');
+    Writeln('Выберите желаемое действие:');
+    textcolor(white);
+    Writeln('1) Добавить элемент');
+    textcolor(cyan);
+    Writeln('2) Вывод стека');
+    textcolor(cyan);
+    Writeln('3) Проверка стека на пустоту');
+    textcolor(cyan);
+    Writeln('4) Очистить стек');
+    textcolor(red);
+    Writeln('5) Взять элемент из Стека');
+    textcolor(red);
+    Writeln('6) Выход');
+    writeln;
+    readln(ch);
+    case ch of 
+      '1': addToStack(Stack);
+      '2':
+        begin
           Clrscr;
           textcolor(LightBlue);
           Write('Cодержимое стека = ');
           Print(Stack);
         end; 
-  '3' : begin
+      '3':
+        begin
           Clrscr;
           textcolor(White);
           WriteLn('Стек пуст = ', top = 0);
         end;
-  '4' : clearStack(Stack);
-  '5' : Pop(Stack);
-end;
-until ch='6';
-
-clearStack(Stack);
+      '4': clearStack(Stack);
+      '5': Pop(Stack);
+    end;
+  until ch = '6';
+  
+  clearStack(Stack);
 end.
